0d154b485d13861339c02b2b8f6adedb281ea1bd
abuh004 <husam.abuhabib@arvato.scs.com>
2023-07-09 22:44:01 +0200

    Implement the Vehicles API

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -1,5 +1,9 @@
 package com.udacity.vehicles;
 
+import com.udacity.vehicles.domain.Condition;
+import com.udacity.vehicles.domain.Location;
+import com.udacity.vehicles.domain.car.Car;
+import com.udacity.vehicles.domain.car.Details;
 import com.udacity.vehicles.domain.manufacturer.Manufacturer;
 import com.udacity.vehicles.domain.manufacturer.ManufacturerRepository;
 import org.modelmapper.ModelMapper;
@@ -63,6 +67,26 @@ public class VehiclesApiApplication {
     @Bean(name="pricing")
     public WebClient webClientPricing(@Value("${pricing.endpoint}") String endpoint) {
         return WebClient.create(endpoint);
+    }
+
+    private Car getCar() {
+        Car car = new Car();
+        car.setLocation(new Location(40.730610, -73.935242));
+        Details details = new Details();
+        Manufacturer manufacturer = new Manufacturer(101, "Chevrolet");
+        details.setManufacturer(manufacturer);
+        details.setModel("Impala");
+        details.setMileage(32280);
+        details.setExternalColor("white");
+        details.setBody("sedan");
+        details.setEngine("3.6L V6");
+        details.setFuelType("Gasoline");
+        details.setModelYear(2018);
+        details.setProductionYear(2018);
+        details.setNumberOfDoors(4);
+        car.setDetails(details);
+        car.setCondition(Condition.USED);
+        return car;
     }
 
 }
